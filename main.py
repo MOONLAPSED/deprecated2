4e4eedfa0b75da61db07a11abef8650d4a3c0085
moonlapsed <MOONLAPSED@gmail.com>
2024-01-27 16:58:58 -0600

    submodule-aware loggins first draft

--- main.py
+++ main.py
@@ -5,7 +5,12 @@ import sys
 
 from src.lager import init_logging
 
+print("The current working directory is: " + os.getcwd())
+
 root_logger, sub_logger = init_logging()
 
 root_logger.info("Log message from main.py")
 sub_logger.info("Another log message from main.py")
+
+from dev.testfiledir import main2
+main2()
